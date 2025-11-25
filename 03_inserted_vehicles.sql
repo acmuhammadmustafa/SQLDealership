1accaa2ebd3c0f07782c0892dd8d42e1b8072e90
Muhammad Mustafa <acmuhammadmustafa@gmail.com>
2025-11-25 13:11:28 -0500

    inserted vehicle info

---
+++ 03_inserted_vehicles.sql
@@ -0,0 +1,5 @@
+INSERT INTO vehicles(VIN, Year, Make, Model, `Vehicle Type`, Color, Odometer, price, Sold) VALUES
+('10112', 1993, 'Ford', 'Explorer', 'SUV', 'Red', 525123, 995.00, FALSE),
+('44901', 2012, 'Honda', 'Civic', 'Sedan', 'Gray', 103221, 6995.00, FALSE),
+('12345', 2020, 'Toyota', 'Camry', 'Sedan', 'Blue', 123000, 8000.00, FALSE),
+('876431', 2023, 'Lexus', 'IS-350 F Sport', 'Sedan', 'Black', 12000, 39000.00, FALSE);
