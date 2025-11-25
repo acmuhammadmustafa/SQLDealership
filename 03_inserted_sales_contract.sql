1e1a1d2078144552aadbee164a6be85a9922b551
Muhammad Mustafa <acmuhammadmustafa@gmail.com>
2025-11-25 13:34:26 -0500

    inserted sale contract

---
+++ 03_inserted_sales_contract.sql
@@ -0,0 +1,4 @@
+INSERT INTO sales_contracts
+(vin, salesTaxAmount, recordingFee, processingFee, financeOption, salesDate, customerName, customerEmail)
+VALUES
+(10112, 49.75, 100.00, 295.00, 'NO', '2021-09-28', 'Dana Wyatt', 'dana@texas.com');
