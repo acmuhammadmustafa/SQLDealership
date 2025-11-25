4ad8fce10013e2352a09fca91c50ee3d83ef76b4
Muhammad Mustafa <acmuhammadmustafa@gmail.com>
2025-11-25 14:03:19 -0500

    fixed inserted dealership

---
+++ 03_inserted_dealerships.sql
@@ -0,0 +1,4 @@
+INSERT INTO dealerships(DealershipsID, DealershipName, Address, Phone) VALUES
+(1, 'D & B Used Cars', '111 Old Benbrook Rd', '817-555-5555'),
+(2, 'M & M New Cars', '123 Chocolate Lane', '123-456-6789'),
+(3, 'Premium Bills Cars', '676 New Street', '222-222-3212');
